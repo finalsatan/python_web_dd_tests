de026aa5bf4f8c706e7aa9e8e0c6d861d643a9dc
finalsatan <zhangyhhust@gmail.com>
2016-02-27 14:53:15 +0800

    Front page HTML now generated from a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -41,9 +41,11 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: 买苹果笔记本' for row in rows)
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            '1: 买苹果笔记本',
+            [row.text for row in rows],
+            "New to-do item did not appear in table"
         )
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 他输入了“用苹果笔记本打魔兽世界”
