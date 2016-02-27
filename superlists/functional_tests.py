41c8eb792cfc0e07cb3f5dc8d95585e2d1ee04f5
finalsatan <zhangyhhust@gmail.com>
2016-02-27 14:27:30 +0800

    Functional test now checks we can input a to-do item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -21,19 +22,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # 他注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 应用邀请他输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # 他在一个文本框中输入了“买苹果笔记本”
         # 小张的爱好是使用苹果笔记本打游戏
+        inputbox.send_keys('买苹果笔记本')
 
         # 他按回车键后，页面更新了
         # 待办事项表格中显示了“1: 买苹果笔记本”
+        inputbox.send_keys(Keys.ENTER)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 买苹果笔记本' for row in rows)
+        )
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 他输入了“用苹果笔记本打魔兽世界”
         # 小张做事很有条理
+        self.fail('Finish the test!')
 
         # 页面再次更新，他的清单中显示了这两个待办事项
 
