2068f6cc44601faef87e14dd2de1fc4a05ad3df5
finalsatan <zhangyhhust@gmail.com>
2016-03-01 21:53:28 +0800

    Add setUpClass and tearDownClass to functional_tests

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'livesever' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super.tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Chrome()
